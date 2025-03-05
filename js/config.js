7109b1804b69726a978e9810eeb54d65e4d322d0
Chaaya Patel <chaayapatel@ucla.edu>
2025-03-04 23:05:29 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_PvQ1dHhN3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'f07dbdc8-691e-4d3e-8da2-d116bab76586', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
